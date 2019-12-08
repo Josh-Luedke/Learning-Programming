2d9bd1ebceecde4175b7eb788ace60ed6d775506
Joshua Luedke <ArchitectureGuy55@gmail.com>
2019-12-08 16:06:37 -0500

    Print-Statements.py

---
+++ Codecademy/Python-2/Syntax/Print-Statements.py
@@ -0,0 +1,4 @@
+# To successfully complete a print statementin python 3 you have to put parenthesis around it
+
+print("Hello, world!")
+print("Deep into the distant woodlands winds a mazy way, reaching to overlapping spurs of mountains bathed in their hill-side blue.")
