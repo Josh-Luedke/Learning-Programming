06a8a22b052574f5fb3f22c4abc147ea827a8a3b
Joshua Luedke <ArchitectureGuy55@gmail.com>
2019-12-29 10:20:17 -0500

    Created Str.py

---
+++ Codecademy/Python-2/String-Console-Output/Str.py
@@ -0,0 +1,7 @@
+# Str will turn things and lines of code that are not strings into strings. 
+
+"""Declare and assign your variable on line 4,
+then call your method on line 5!"""
+
+pi = 3.14
+print str(pi)
