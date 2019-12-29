2764509573b5d657ef70c6125aed20c472ce2248
Joshua Luedke <ArchitectureGuy55@gmail.com>
2019-12-29 10:16:20 -0500

    Created Numbers.py

---
+++ Codecademy/Python-2/Syntax/Numbers.py
@@ -0,0 +1,7 @@
+# In python variables commonly hold some sort of numerical value. If one of these numbers has a decimal it is called a float
+
+cucumbers = 3
+price_per_cucumber = 3.25
+
+total_cost = cucumbers * price_per_cucumber
+print (total_cost)
