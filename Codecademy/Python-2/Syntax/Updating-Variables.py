34faab6f4f833d2b4589089f46392267b257114c
Joshua Luedke <ArchitectureGuy55@gmail.com>
2019-12-29 10:13:27 -0500

    Created Updating-Variables.py

---
+++ Codecademy/Python-2/Syntax/Updating-Variables.py
@@ -0,0 +1,9 @@
+# By using arithmetic we can change and update variables as we go in the program
+
+
+september_rainfall = 5.16
+october_rainfall = 7.20
+november_rainfall = 5.06
+december_rainfall = 4.06
+
+annual_rainfall += september_rainfall + october_rainfall + november_rainfall + december_rainfall
