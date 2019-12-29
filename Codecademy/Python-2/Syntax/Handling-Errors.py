eb394a6c71b8984f1f0efaeb9775993c539a6ee4
Joshua Luedke <ArchitectureGuy55@gmail.com>
2019-12-29 10:17:23 -0500

    Created Handling-Errors.py

---
+++ Codecademy/Python-2/Syntax/Handling-Errors.py
@@ -0,0 +1,4 @@
+# Errors are quite common in the programing world and it is a good habit and somewhat necessary for you to be able to understand these errors 
+
+print('How do you make a hot dog stand?')
+print("You take away its chair!")
