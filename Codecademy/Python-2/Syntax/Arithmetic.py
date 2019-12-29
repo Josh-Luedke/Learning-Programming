376d5f8e15dfe29666d7fd4bb0245788fb70e39b
Joshua Luedke <ArchitectureGuy55@gmail.com>
2019-12-29 10:18:22 -0500

    Created Arithmetic.py

---
+++ Codecademy/Python-2/Syntax/Arithmetic.py
@@ -0,0 +1,4 @@
+# Programs generally need some sort of arithmetic and luckily computers are great at this, python can run subtraction (-), addition (+), multiplication (*), and division (/) or (%)
+
+product = 41 * 7
+remainder = 1398 % 11
