823a7e88f88e47f2c38607bc74f509f15b79b1af
Joshua Luedke <ArchitectureGuy55@gmail.com>
2019-12-29 10:17:41 -0500

    Created Comments.py

---
+++ Codecademy/Python-2/Syntax/Comments.py
@@ -0,0 +1,5 @@
+# Comments allow for programers to write reminders or create a better understanding of the code and program
+
+city_name = "St. Potatosburg"
+# The amount of people living in the city of St. Potatosburg: 
+city_pop = 340000
