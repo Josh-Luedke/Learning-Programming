4d49483c10e54e8fd3ae828bb83d160951ec556d
Joshua Luedke <ArchitectureGuy55@gmail.com>
2019-12-29 10:21:38 -0500

    Created Escaping-Characters,py

---
+++ Codecademy/Python-2/String-Console-Output/Escaping-Characters.py
@@ -0,0 +1,5 @@
+# Escaping Characters are strings that are flawed because some characters can cause a string to break.
+
+# The string below is broken. Fix it using the escape backslash!
+
+'This isn\'t flying, this is falling with style!'
