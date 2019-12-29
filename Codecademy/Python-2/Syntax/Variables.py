6c62ec80fc34c27634f0af8ad7559ca398e2d6ad
Joshua Luedke <ArchitectureGuy55@gmail.com>
2019-12-29 10:11:14 -0500

    Created Variables.py

---
+++ Codecademy/Python-2/Syntax/Variables.py
@@ -0,0 +1,3 @@
+# There are certian data types that may change through out the duration of a program and at differnet times, hence variables are required in Python for these factors.  
+
+todays_date = "December-13" 
