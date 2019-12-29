8f66c3f861908ec413692b860dbc2e24f3e5d589
Joshua Luedke <ArchitectureGuy55@gmail.com>
2019-12-29 10:19:20 -0500

    Created String-Concatenation.py

---
+++ Codecademy/Python-2/String-Console-Output/String-Concatenation.py
@@ -0,0 +1,5 @@
+# String Concatenation allows for coders to combine strings and arithmetic in Python. 
+
+# Print the concatenation of "Spam and eggs" on line 3!
+
+print ("Spam ") + ("and ") + ("eggs")
