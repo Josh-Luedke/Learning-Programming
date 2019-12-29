3caac979ab2126e67805c8378a5aa21ead41fedc
Joshua Luedke <ArchitectureGuy55@gmail.com>
2019-12-29 10:12:25 -0500

    Created Value-Error.py

---
+++ Codecademy/Python-2/Syntax/Value-Error.py
@@ -0,0 +1,7 @@
+# ValueError allows for us to convert variables into different datatypes. 
+
+float_1 = 0.25
+float_2 = 40.0
+
+product = float_1 * float_2
+big_string = "The product was " + str(product)
