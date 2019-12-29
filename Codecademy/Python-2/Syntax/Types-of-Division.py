185f25b8fb0d45935bcfa0425e23c8e9a572f86d
Joshua Luedke <ArchitectureGuy55@gmail.com>
2019-12-29 10:15:29 -0500

    Created Types-of-Division.py

---
+++ Codecademy/Python-2/Syntax/Types-of-Division.py
@@ -0,0 +1,10 @@
+# In a division problem the quotient is often rounded to a whole number when divided in python. If you make the numbers you are dividing floats than will also be a float. 
+
+cucumbers = 100
+num_people = 6
+
+whole_cucumbers_per_person = cucumbers / num_people
+
+float_cucumbers_per_person = float(cucumbers) / num_people
+
+print ("float_cucumbers_per_person")
