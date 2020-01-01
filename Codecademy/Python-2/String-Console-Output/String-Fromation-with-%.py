ba2683d152e18b12cac9a8a565f0b9ab89eadc25
Joshua Luedke <ArchitectureGuy55@gmail.com>
2019-12-31 20:01:26 -0500

    Created String-Formation-with-%.py

---
+++ Codecademy/Python-2/String-Console-Output/String-Fromation-with-%.py
@@ -0,0 +1,15 @@
+# The % symbol is a way of combining a string with variables.
+
+string_1 = "Camelot"
+string_2 = "place"
+
+print "Let's not go to %s. 'Tis a silly %s." % (string_1, string_2)
+
+# The % operator replaces %s placeholders with variables within paranthesis
+
+name = raw_input("What is your name? ")
+quest = raw_input("What is your quest? ")
+color = raw_input("What is your favorite color? ")
+
+print "Ah, so your name is %s, your quest is %s, " \
+"and your favorite color is %s." % (name, quest, color)
