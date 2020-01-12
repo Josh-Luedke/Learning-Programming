350ba8ff80b90e86c0e2424601cb2c72fc7e1c12
Joshua Luedke <ArchitectureGuy55@gmail.com>
2020-01-11 21:02:19 -0500

    Created Extracting-Info.py

---
+++ Codecademy/Python-2/Date-&-Time/Extracting-Info.py
@@ -0,0 +1,8 @@
+# You can use year, month, and day functions in order to find the entire date. 
+
+from datetime import datetime
+now = datetime.now()
+
+current_year = now.year
+current_month = now.month
+current_day = now.day
