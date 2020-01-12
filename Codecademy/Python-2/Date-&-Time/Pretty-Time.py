b800ab46f58db46700a92dd4c8209bf64fea2a4f
Joshua Luedke <ArchitectureGuy55@gmail.com>
2020-01-11 21:03:27 -0500

    Created Pretty-Time.py

---
+++ Codecademy/Python-2/Date-&-Time/Pretty-Time.py
@@ -0,0 +1,6 @@
+# Use this to print the hour, minute, and second. 
+
+from datetime import datetime
+now = datetime.now()
+
+print '%02d/%02d/%04d %02d:%02d:%02d' % (now.month, now.day, now.year, now.hour, now.minute, now.second)
